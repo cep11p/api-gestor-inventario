1c2fb8529b551c8ccc0bbf79d33ef630bb516a37
carlos <cep11p@gmail.com>
2020-06-04 15:11:29 -0300

    TG-60 - TG-61 #Closed - TG-62 #Closed Se realiza el fixture de unidad de medida https://tree.taiga.io/project/areangonzalez-gestor-de-inventario/us/60?milestone=266798

---
+++ tests/_data/unidad_medida.php
@@ -0,0 +1,8 @@
+<?php
+
+return [   
+    'unidad_medida1'=>['id'=>1,'nombre'=>'Kilogramo','simbolo'=>'Kl'],
+    'unidad_medida2'=>['id'=>2,'nombre'=>'Gramo','simbolo'=>'Gr'],
+    'unidad_medida3'=>['id'=>3,'nombre'=>'Litro','simbolo'=>'L'],
+    'unidad_medida4'=>['id'=>4,'nombre'=>'Mililitro','simbolo'=>'Ml'],
+];
