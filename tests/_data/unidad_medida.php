44a2a5609374eb57eba83159df0f5d42f1f180df
carlos <cep11p@gmail.com>
2020-06-04 15:25:24 -0300

    TG-60 Se agrega unidad como unidad de medida https://tree.taiga.io/project/areangonzalez-gestor-de-inventario/us/60?milestone=266802

--- tests/_data/unidad_medida.php
+++ tests/_data/unidad_medida.php
@@ -5,4 +5,5 @@ return [
     'unidad_medida2'=>['id'=>2,'nombre'=>'Gramo','simbolo'=>'Gr'],
     'unidad_medida3'=>['id'=>3,'nombre'=>'Litro','simbolo'=>'L'],
     'unidad_medida4'=>['id'=>4,'nombre'=>'Mililitro','simbolo'=>'Ml'],
+    'unidad_medida4'=>['id'=>4,'nombre'=>'Unidad','simbolo'=>'U'],
 ];
