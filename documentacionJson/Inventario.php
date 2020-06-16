1ed96dd52fd2e9945871dce8c02e675c469d0ee8
carlos <cep11p@gmail.com>
2020-06-16 14:24:34 -0300

    TG-81 - TG-98 #Closed Se la documentacionJson https://tree.taiga.io/project/areangonzalez-gestor-de-inventario/us/81?milestone=267214

--- documentacionJson/Inventario.php
+++ documentacionJson/Inventario.php
@@ -11,6 +11,18 @@
 * @url http://api.gestor-inventario.local/inventarios 
 * @method POST
 * @param arrayJson
+    {
+	"nro_remito":"0001-00001",
+	"fecha_emision":"2020-03-15",
+	"total":292.99,
+	"descripcion":"esto es una descripcion del stock entrante",
+	"lista_producto":[
+		{"id":1,"fecha_vencimiento":"2020-10-10","precio_unitario":120,"defectuoso":1,"cantidad":10},
+		{"id":1,"fecha_vencimiento":"2020-10-10","precio_unitario":120,"falta":1,"cantidad":10},
+		{"id":1,"fecha_vencimiento":"2020-10-10","precio_unitario":120,"cantidad":100},
+		{"id":1,"fecha_vencimiento":"2020-11-11","precio_unitario":120,"cantidad":50}
+	]
+    }
 **/
 
 /**** Para modificar*****
