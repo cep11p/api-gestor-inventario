2107947e3c6a28f4fec54daefd970d0e1fa029f1
carlos <cep11p@gmail.com>
2020-06-23 23:54:50 -0300

    TG-104 Si las vaciones del filtrado son falsas se obtiene un resultado vacio https://tree.taiga.io/project/areangonzalez-gestor-de-inventario/us/104?milestone=267846

--- models/InventarioSearch.php
+++ models/InventarioSearch.php
@@ -150,7 +150,7 @@ class InventarioSearch extends Inventario
 
         if (!$this->validate()) {
             // uncomment the following line if you do not want to any records when validation fails
-            // $query->where('0=1');
+             $query->where('0=1');
             return $dataProvider;
         }
     
