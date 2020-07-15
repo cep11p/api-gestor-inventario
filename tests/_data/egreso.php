691af2e28700c36559eb535add55c43c38218b94
carlos <cep11p@gmail.com>
2020-07-15 09:43:37 -0300

    TG-219 - TG-221 #Closed - TG-222 #Closed Se crea la tabla y el modelo de tipo_egreso https://tree.taiga.io/project/areangonzalez-gestor-de-inventario/us/219?no-milestone=1

--- tests/_data/egreso.php
+++ tests/_data/egreso.php
@@ -1,7 +1,7 @@
 <?php
 
 return [   
-    'egreso1'=>['id'=>1,'fecha'=>'2019-03-03','nro_acta'=>'0001','origen'=>"un origen",'destino_nombre'=>'Un destino','destino_localidadid'=>2626,'descripcion'=>'Esto es un egreso1 creado con fixture'],
-    'egreso2'=>['id'=>2,'fecha'=>'2019-04-04','nro_acta'=>'0002','origen'=>"un origen",'destino_nombre'=>'Un destino','destino_localidadid'=>2626,'descripcion'=>'Esto es un egreso2 creado con fixture'],
-    'egreso3'=>['id'=>3,'fecha'=>'2019-05-05','nro_acta'=>'0003','origen'=>"un origen",'destino_nombre'=>'Un destino','destino_localidadid'=>2626,'descripcion'=>'Esto es un egreso3 creado con fixture'],
+    'egreso1'=>['id'=>1,'fecha'=>'2019-03-03','nro_acta'=>'0001','origen'=>"origen1",'destino_nombre'=>'destino1','destino_localidadid'=>2626,'descripcion'=>'Esto es un egreso1 creado con fixture1'],
+    'egreso2'=>['id'=>2,'fecha'=>'2019-04-04','nro_acta'=>'0002','origen'=>"origen2",'destino_nombre'=>'destino2','destino_localidadid'=>2626,'descripcion'=>'Esto es un egreso2 creado con fixture2'],
+    'egreso3'=>['id'=>3,'fecha'=>'2019-05-05','nro_acta'=>'0003','origen'=>"origen3",'destino_nombre'=>'destino3','destino_localidadid'=>2626,'descripcion'=>'Esto es un egreso3 creado con fixture3'],
 ];
